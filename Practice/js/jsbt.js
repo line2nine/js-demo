cab1857a5dab0491028e32775f95f877834d3ca6
line2nine <hailinh203@gmail.com>
2020-03-06 10:50:51 +0700

    Add files

---
+++ Practice/js/jsbt.js
@@ -0,0 +1,15 @@
+function showMessage(){
+    alert('Xin chào bạn!');
+}
+
+function hello(){
+    document.getElementById('hello').innerHTML = 'Xin chào các bạn!';
+}
+
+function showMessageHello(){
+    document.write('Xin chào các bạn!');
+}
+
+function showMessageConsole(){
+    console.log('Xin chào!');
+}
